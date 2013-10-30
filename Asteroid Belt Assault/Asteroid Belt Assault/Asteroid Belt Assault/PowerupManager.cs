8f7181e7d303c6cf9820b6fa819679d0cdd37759
smuckers <zachmunkirs@hotmail.com>
2013-10-30 08:46:33 -0400

    did powerup

--- Asteroid Belt Assault/Asteroid Belt Assault/Asteroid Belt Assault/PowerupManager.cs
+++ Asteroid Belt Assault/Asteroid Belt Assault/Asteroid Belt Assault/PowerupManager.cs
@@ -12,22 +12,25 @@ namespace Asteroid_Belt_Assault
         Texture2D spriteSheet;
         private PlayerManager playerManager;
         public List<Sprite> Powerups = new List<Sprite>();
+        public Sprite powerups;
+        private bool newPowerup;
 
         public PowerupManager(Texture2D spriteSheet, PlayerManager playerManager)
         {
             this.spriteSheet = spriteSheet;
             this.playerManager = playerManager;
 
-            Powerups.Add(Sprite newPowerup) = new Sprite(new Vector2(300, 300), spriteSheet, new Rectangle(355, 181, 97, 93), Vector2.Zero);
+            Powerups.Add(new Sprite(new Vector2(300, 300), spriteSheet, new Rectangle(355, 181, 97, 93), Vector2.Zero));
         }
 
-        public void Update(GameTime gametime)
+        public void Update(GameTime gameTime)
         {
-
+           powerups.Update(gameTime);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
+            powerups.Draw(spriteBatch);
         }
     }
 }
